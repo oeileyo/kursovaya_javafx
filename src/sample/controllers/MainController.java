bf8a159886dd92b420c63d7e1385e2b11ec30458
Nastya <nastyadoma@mail.ru>
2021-04-19 23:28:35 +0300

    19/04

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -1,4 +1,27 @@
 package sample.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+    public static void show() {
+    FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainController.class.getResource("/sample/views/main.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        };
 }
--- src/sample/controllers/SignInController.java
+++ src/sample/controllers/SignInController.java
@@ -5,14 +5,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import sample.exceptions.AppException;
 
-public class SignInController {
+public class SignInController implements Initializable {
+
+    @FXML
+    private Label message;
 
     @FXML
     private ResourceBundle resources;
@@ -29,25 +36,54 @@ public class SignInController {
     @FXML
     private PasswordField password_field;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { }
+
+    private void attachEvent() {
+        login_field.getScene().setOnKeyPressed(event -> { if (event.getCode() == KeyCode.ENTER) { login(); } });
+    }
+
     @FXML
-    void initialize() {
-        login_button.setOnAction(event ->{
-            System.out.println("pip");
-            login_button.getScene().getWindow().hide();
+    private void login() {
+        try {
+            String login_ = "admin";
+            String password_ = "admin";
 
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/sample/main.fxml"));
-
-            try {
-                loader.load();
-            } catch (IOException e){
-                e.printStackTrace();
+            if (login_field.getText().equals(login_) && password_field.getText().equals(password_)) {
+                MainController.show(); //входим
+                cancel(); //закрываем
+            } if (login_field.getText().isEmpty()) {
+                throw new AppException("Поле ввода логина должно быть заполнено.");
+            } if (password_field.getText().isEmpty()) {
+                throw new AppException("Поле ввода логина должно быть заполнено.");
+            } else {
+                throw new AppException("Введен неверный логин или пароль.");
             }
+        }
+        catch (AppException e) {
+            message.setText(e.getMessage());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            cancel();
+        }
+    }
 
-            Parent root = loader.getRoot();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.showAndWait();
-        });
+    @FXML
+    private void cancel() { login_button.getScene().getWindow().hide(); }
+
+    public static void loadView(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(SignInController.class.getResource("./views/login.fxml"));
+            Parent view = loader.load();
+            stage.setScene(new Scene(view));
+
+            SignInController controller = loader.getController();
+            controller.attachEvent();
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
